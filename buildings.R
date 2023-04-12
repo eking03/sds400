96e212a3259756a6e223b7cf9004806e76fae940
eking03 <eking03@smith.edu>
2023-04-12 00:10:20 -0400

    Updated region of interest from campus-level to neighborhood-level, using the quad as reference, successfully conserving memory and greatly reducing runtime. Generated roof coordinates for the four distinct buildings.

--- buildings.R
+++ buildings.R
@@ -8,7 +8,7 @@ campus_footprints <- st_read("C:/Users/jaria/Documents/R/Capstone-410/sds400/Cam
 
 buildings_2017 <- st_read("C:/Users/jaria/Documents/R/Capstone-410/sds400/buildings_2017/buildings_2017.shp") %>%
   st_transform(8748) %>%
-  st_crop(c(xmin = 345214.82, ymin = 2940022.52, xmax = 350525.83, ymax = 2947382.60))
+  st_crop(c(xmin = 345214.82, ymin = 2940022.52, xmax = 350525.83, ymax = 2945560.64))
 
 overlap <- st_overlaps(buildings_2017, campus_footprints)
 buildings_2017$smith <- map_lgl(overlap, function(x) {
@@ -19,10 +19,17 @@ buildings_2017$smith <- map_lgl(overlap, function(x) {
   }
 })
 
-ggplot() +
-  geom_sf(data = buildings_2017, aes(fill = smith)) + 
-  #geom_sf(data = campus_footprints) +
-  coord_sf()
 
-#ggplot(buildings_2017) + geom_sf() + coord_sf()
-ggplot(campus_footprints) + geom_sf() + coord_sf()
+roof <- campus_footprints %>%
+  filter(OBJECTID == 116 | OBJECTID == 131 | OBJECTID == 141 | OBJECTID == 148 ) %>%
+  st_point_on_surface()
+
+ggplot() +
+  geom_sf(data = campus_footprints, fill = "white", alpha = 0.8) +
+  geom_sf(data = roof) +
+  coord_sf(
+    xlim = c(346242.477, 347338.812), 
+    ylim = c(2943359.757, 2944438.415),
+    expand = FALSE,
+    crs = st_crs(8748))
+
--- lidr_practice.R
+++ lidr_practice.R
@@ -8,13 +8,15 @@ las.dir <- "C:/Users/jaria/Downloads/2015_ME_MA_lidar_Job847219/"
 file.names <- list.files(path = las.dir, pattern = "Job847219_42072_*", full.names = TRUE)
 
 
-las67 <- lidR::readLAScatalog(file.names[c(6, 7)], progress = FALSE, filter = "-keep_first -drop_z_below 0 -drop_z_above 500")
-las <- lidR::clip_rectangle(las67, 345214.82, 2940022.52, 350525.83, 2947382.60)
+#las67 <- lidR::readLAScatalog(file.names[c(6, 7)], progress = FALSE, filter = "-keep_first -drop_z_below 0 -drop_z_above 500")
+#las <- lidR::clip_rectangle(las67, 345214.82, 2940022.52, 350525.83, 2945560.64)
 
-lidR::plot(las, pal = viridis::magma(24), bg = "white", axis = FALSE, legend = TRUE)
+las7 <- lidR::readLAScatalog(file.names[7], progress = FALSE, filter = "-keep_first -drop_z_below 0 -drop_z_above 500")
+lasQuad <- lidR::clip_rectangle(las7, 346242.477, 2943359.757, 347338.812, 2944438.415)
+lasQuad <- lidR::filter_poi(lasQuad, Z < 350)
 
-#lidR::plot(las, color = "Classification", bg = "white", axis = FALSE, legend = TRUE)
-#las.norm <- lidR::normalize_height(las, algorithm = lidR::tin(), na.rm = TRUE, res = 1)
+lidR::plot(lasQuad, pal = viridis::magma, bg = "white", axis = FALSE, legend = TRUE)
+
 
 
 
